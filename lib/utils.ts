0cdf24808eb6eac2e270c897067089bf9bb3eeec
seblas81 <seblas@gmail.com>
2025-12-25 22:33:08 -0300

    Limpiando duplicados en utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,9 +6,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
